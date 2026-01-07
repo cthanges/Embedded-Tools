ec70665036c0dfaa7377d93003fe8a1226ce9f03
Charran Thangeswaran <charranthangeswaran@gmail.com>
2026-01-07 13:49:24 -0500

    Add a function for handling transitions among states

--- Embedded-FSM-Simulator/fsm.py
+++ Embedded-FSM-Simulator/fsm.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from events import Event
 
 class State(Enum):
     BOOT = 0
@@ -7,3 +8,42 @@ class State(Enum):
     RUNNING = 3
     ERROR = 4
     SHUTDOWN = 5
+
+class FSM:
+    def __init__(self):
+        self.state = State.BOOT
+
+    def on_event(self, event):
+        old_state = self.state
+
+        if self.state == State.BOOT:
+            if event == Event.POWER_ON:
+                self.state = State.INIT
+
+        elif self.state == State.INIT:
+            if event == Event.INIT_COMPLETE:
+                self.state = State.IDLE
+            elif event == Event.ERROR_DETECTED:
+                self.state = State.ERROR
+
+        elif self.state == State.IDLE:
+            if event == Event.START_COMMAND:
+                self.state = State.RUNNING
+            elif event == Event.ERROR_DETECTED:
+                self.state = State.ERROR
+            elif event == Event.SHUTDOWN:
+                self.state = State.SHUTDOWN
+
+        elif self.state == State.RUNNING:
+            if event == Event.ERROR_DETECTED:
+                self.state = State.ERROR
+            elif event == Event.SHUTDOWN:
+                self.state = State.SHUTDOWN
+
+        elif self.state == State.ERROR:
+            if event == Event.RESET:
+                self.state = State.INIT
+            elif event == Event.SHUTDOWN:
+                self.state = State.SHUTDOWN
+
+        return self.state

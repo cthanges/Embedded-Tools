3e472fab9bd3b06ab7f4a993814651f397b3bbd6
Charran Thangeswaran <charranthangeswaran@gmail.com>
2026-01-07 13:14:40 -0500

    Define events

---
+++ Embedded-FSM-Simulator/events.py
@@ -0,0 +1,7 @@
+class Event:
+    POWER_ON = "power_on"
+    INIT_COMPLETE = "init_complete"
+    START_COMMAND = "start_command"
+    ERROR_DETECTED = "error_detected"
+    RESET = "reset"
+    SHUTDOWN = "shutdown"

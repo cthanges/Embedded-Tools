54c5e52bc241f73e7a2712a17f8c673a09094e7c
Charran Thangeswaran <charranthangeswaran@gmail.com>
2026-01-19 11:41:35 -0500

    Add functions for parsing and analyzing log entries

--- UART-Log-Parser/main.c
+++ UART-Log-Parser/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 typedef struct{
     int timestamp;
@@ -10,4 +11,34 @@ typedef struct{
 int main(){
 
     return 0;
+}
+
+// Parse a single log line (use int as return type for success/failure)
+int parse_line(const char *line, log_entry_t *entry) 
+{
+    return sscanf(line,
+                  "[%d][%7[^]]][%15[^]] %[^\n]",
+                  &entry->timestamp,
+                  entry->level,
+                  entry->module,
+                  entry->message) == 4;
+}
+
+// Analyze parsed log entries (Only considering error and warning levels for the summary report)
+void analyze_logs(log_entry_t *entries, int count, int *errors, int *warnings, char *last_fsm_state)
+{
+    *errors = 0;
+    *warnings = 0;
+    last_fsm_state[0] = '\0';
+
+    for (int i = 0; i < count; i++) {
+        if (strcmp(entries[i].level, "ERROR") == 0)
+            (*errors)++; // Dereference pointer to increment
+        else if (strcmp(entries[i].level, "WARN") == 0)
+            (*warnings)++; // Dereference pointer to increment
+
+        if (strcmp(entries[i].module, "FSM") == 0) {
+            strcpy(last_fsm_state, entries[i].message);
+        }
+    }
 }
